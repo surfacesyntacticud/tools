426dc1ba2b6649e52a10f8010f8a304c7d3dd470
Bruno Guillaume <Bruno.Guillaume@loria.fr>
2023-02-28 22:49:33 +0100

    Add python script for stat_pos_features

---
+++ grewpy/README.md
@@ -0,0 +1,21 @@
+# Some Python scripts on SUD data
+
+We suppose that `grewpy` version 0.2.0 and `grewpy_backend` version 0.2.0 are installed ([Install doc](https://grew.fr/usage/python)).
+
+## stat_pos_features.py
+
+This script produces a table showing for each UPOS and for each feature name, the ratio of token with this UPOS who have the given feature name. 
+
+With `$GSD` set on a folder containing **UD_French-GSD** data, this [table](stat_pos_features/SUD_French-GSD.md) is produced by the command:
+
+```
+python3 stat_pos_features.py $GSD/*.conllu > stat_pos_features/UD_French-GSD.md
+```
+
+With data updated on 2023/02/28, the following tables are available:
+ - [SUD_Beja-NSC.md](stat_pos_features/SUD_Beja-NSC.md)
+ - [SUD_French-GSD.md](stat_pos_features/SUD_French-GSD.md)
+ - [SUD_French-ParisStories.md](stat_pos_features/SUD_French-ParisStories.md)
+ - [SUD_French-Rhapsodie-GSD.md](stat_pos_features/SUD_French-Rhapsodie-GSD.md)
+ - [SUD_Naija-NSC.md](stat_pos_features/SUD_Naija-NSC.md)
+ - [SUD_Zaar-Autogramm.md](stat_pos_features/SUD_Zaar-Autogramm.md)
---
+++ grewpy/stat_pos_features.py
@@ -0,0 +1,59 @@
+import sys
+from grewpy import CorpusDraft, Request, Corpus, set_config
+set_config("sud")
+
+ud_tagset = [
+    "ADJ", "ADP", "ADV", "AUX", "CCONJ", "DET", 
+    "INTJ", "NOUN", "NUM", "PART", "PRON", "PROPN", 
+    "PUNCT", "SCONJ", "SYM", "VERB", "X"
+    ]
+
+def keep (f):
+  """
+  decide which features should be kept in the final table
+  """
+  if f.startswith("Correct") or f.startswith("Align"):
+    # Remove features related errors
+    return False
+  elif f.lower() == f:
+    # Remove "special" features form, lemma, upos, xpos, wordform and textform
+    return False
+  elif f in ["SpaceAfter", "Title", "InTitle", "Idiom", "InIdiom", "__RAW_MISC__"]:
+    # Feature not related to POS of the token
+    return False
+  else:
+    # Keep everthing else
+    return True
+
+if __name__ == "__main__":
+  corpus_files = sys.argv[1:]
+  corpus = Corpus(corpus_files)
+
+  # NB: we have to iterate on all graph -> CorpusDraft is much more efficient for this
+  corpus_draft = CorpusDraft(corpus)
+  all_features = set()
+  # collect all features names used in the Treebank
+  for sent_id in corpus_draft:
+    graph = corpus_draft[sent_id]
+    for gid in graph.features:
+      fs = graph.features[gid]
+      for feat_name in fs:
+        all_features.add (feat_name)
+
+  # keep only interesting ones
+  features = [f for f in list(all_features) if keep (f)]
+
+  # print the table in Markdown syntax
+  print (" |  | "+ " | ".join(features))
+  print ("|---|"+ "---|" * len(features))
+  for upos in ud_tagset:
+    print (f' | {upos} ', end="")
+    total = corpus.count(Request(f'N[upos={upos}]'))
+    for feat in features:
+      yes = corpus.count(Request(f'N[upos={upos}, {feat}]'))
+      if yes == 0:
+        print (' | ', end="")
+      else: 
+        print (' | %.2f%%' % (yes/total*100), end="")
+    print()
+
