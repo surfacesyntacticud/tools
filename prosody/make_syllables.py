f827d622492bf2ff22e12a356a59f11a58117e50
Bruno Guillaume <Bruno.Guillaume@loria.fr>
2024-05-03 11:20:10 +0200

    code_review

--- prosody/make_syllables.py
+++ prosody/make_syllables.py
@@ -7,45 +7,39 @@ def parse_misc(m):
     d[feat[0:e]] = feat[e+1:]
   return d
 
-infile = sys.argv[1]
-with open(infile, 'r') as f:
+with open(sys.argv[1], 'r') as f:
   lines = f.readlines()
 
-outfile = sys.argv[2] if len(sys.argv) > 2 else None
-with open(outfile, "w") if outfile else sys.stdout as f:
+with open(sys.argv[2], "w") if len(sys.argv)>2 else sys.stdout as f:
   for line in lines:
-    l = line.strip()
-    if l == "":
-      f.write(l+"\n")
-    elif l[0] == "#":
-      f.write(l+"\n")
+    if line.strip() == "" or line[0] == "#":
+      f.write(line)
     else:
-      fields = line.split("\t")
+      fields = line.strip().split("\t")
       feats = parse_misc(fields[9])
-      reduced_feats = { k: feats[k] for k in feats if not k.startswith("Syl") }
-      misc = "|".join([f"{k}={reduced_feats[k]}" for k in reduced_feats])
-      f.write("\t".join(fields[0:9])+"\t"+misc.strip()+"\n")
-      cont = True
-      pos = 1
-      while cont:
-        prefix = f"Syl{pos}"
-        subfeats = { k: feats[k] for k in feats if k.startswith(prefix) }
-        if len(subfeats) == 0:
-          cont=False
+      word_feats = { k: feats[k] for k in feats if not k.startswith("Syl") }
+      word_misc = "|".join([f"{k}={word_feats[k]}" for k in word_feats])
+      f.write("\t".join(fields[0:9])+"\t"+word_misc+"\n")
+      done = False
+      syl_number = 1
+      while not done:
+        prefix = f"Syl{syl_number}"
+        syl_feats = { k: feats[k] for k in feats if k.startswith(prefix) }
+        if len(syl_feats) == 0:
+          done=True
         else:
-          misc_list = [f"{k[4:]}={subfeats[k]}" for k in subfeats if k != prefix]
-          misc = "|".join(misc_list)
+          syl_misc = "|".join([f"{k[4:]}={syl_feats[k]}" for k in syl_feats if k != prefix])
           # Some numeric features can occasionaly have value "X" like "MeanF0=X"
           # This value is not accepted by Grew, so it is replaced by "MeanF0_X=Yes"
-          misc = misc.replace("MeanF0=X", "MeanF0_X=Yes")
-          misc = misc.replace("Duration=X", "Duration_X=Yes")
-          misc = misc.replace("MaxAmplitude=X", "MaxAmplitude_X=Yes")
-          misc = misc.replace("AvgAmplitude=X", "AvgAmplitude_X=Yes")
-          if misc == "":
-            misc = "_"
-          syl_id = f"{fields[0]}.{pos}"
-          form = subfeats.get(prefix, "__undef__").strip()
-          if form == "":
-            form = "__empty__"
-          f.write(f"{syl_id}\t_\t_\t_\t_\tSylForm={form}\t{fields[0]}\tSyl={pos}\t_\t{misc}\n")
-          pos += 1
+          syl_misc = syl_misc.replace("MeanF0=X", "MeanF0_X=Yes")
+          syl_misc = syl_misc.replace("Duration=X", "Duration_X=Yes")
+          syl_misc = syl_misc.replace("MaxAmplitude=X", "MaxAmplitude_X=Yes")
+          syl_misc = syl_misc.replace("AvgAmplitude=X", "AvgAmplitude_X=Yes")
+          if syl_misc == "":
+            syl_misc = "_"
+          syl_id = f"{fields[0]}.{syl_number}"
+          syl_form = syl_feats.get(prefix, "__undef__")
+          if syl_form == "":
+            syl_form = "__empty__"
+          f.write(f"{syl_id}\t_\t_\t_\t_\tSylForm={syl_form}\t{fields[0]}\tSyl={syl_number}\t_\t{syl_misc}\n")
+          syl_number += 1
